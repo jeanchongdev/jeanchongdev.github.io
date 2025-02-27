9ce96cc2c3bca748a6a76a160d6867336e58d4f1
jeanchongdev <grarenajeanpro@gmail.com>
2025-02-26 19:07:01 -0500

    Actualización del cursor1

--- JS/cursor.js
+++ JS/cursor.js
@@ -7,28 +7,31 @@ const isTouchDevice = () => {
 if (!isTouchDevice()) {
     const cursor = document.getElementById('cursorr');
 
-    // Array de colores para las estrellas
-    const colors = ['#25E7AF'];
+// Array de colores para las estrellas
+const colors = ['#25E7AF'];
 
     document.addEventListener('mousemove', (e) => {
-        // Mueve el cursor personalizado
-        cursor.style.left = `${e.pageX}px`;
-        cursor.style.top = `${e.pageY}px`;
+    // Mueve el cursor personalizado
+    cursor.style.left = `${e.pageX}px`;
+    cursor.style.top = `${e.pageY}px`;
 
-        // Crea las "estrellas" con colores aleatorios
-        const star = document.createElement('div');
-        star.classList.add('star');
-        star.style.left = `${e.pageX}px`;
-        star.style.top = `${e.pageY}px`;
-        star.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]; // Color aleatorio
-        document.body.appendChild(star);
+    // Crea las "estrellas" con colores aleatorios
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.left = `${e.pageX}px`;
+    star.style.top = `${e.pageY}px`;
+    star.style.backgroundColor = colors[Math.
+    floor(Math.random() * colors.length)]; 
+    // Color aleatorio
+    document.body.appendChild(star);
 
-        // Elimina la estrella después de un tiempo
-        setTimeout(() => {
-            star.remove();
-        }, 1000);
-    });
-} else {
-    // Opcional: puedes establecer un cursor normal para dispositivos táctiles
+    // Elimina la estrella después de un tiempo
+    setTimeout(() => {
+        star.remove();
+    }, 1000);
+});
+    } else {
+    // Opcional: puedes establecer un
+    //  cursor normal para dispositivos táctiles
     document.body.style.cursor = 'default';
 }
